66367a5fbbe0bc4113124adae1898a177c193385
Shikha-Venaai <shikha.py36@gmail.com>
2024-04-18 13:47:31 +0530

    new setup cleanup

--- Fundamentels/identifiers/file2/file2.go
+++
@@ -1,6 +0,0 @@
-//file2.go
-
-package file2
-
-// Exported variable
-var AnotherExportedVariable = "Jai Shree Krishna!"
--- Fundamentels/identifiers/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Shikha-code36/golang-tutorial-practice/Fundamentels/identifiers/file2
-
-go 1.22.2
